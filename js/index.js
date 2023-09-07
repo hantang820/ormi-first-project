9b6d7e51bb385adba567273a8f29dd1407670bbe
hantang820 <142385695+hantang820@users.noreply.github.com>
2023-09-07 15:43:28 +0900

    Delete js directory

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function ToResultPage() {
-  const inputTempo = document.querySelector('input[name="tempo"]:checked').value;
-  const inputSeason = document.querySelector('input[name="season"]:checked').value;
-  const inputSound = document.querySelector('input[name="sound"]:checked').value;
-
-  window.location.href = `result.html?tempo=${inputTempo}&season=${inputSeason}&sound=${inputSound}`;
-}
